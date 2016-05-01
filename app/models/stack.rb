9e922f4244f0d19184338f7c3567777e9ad3910b
Rudy Rigot <rudy.rigot@gmail.com>
2016-05-01 12:55:15 -0700

    Tool::self.sync_from_stackshare_api and Stack::self.sync_from_stackshare_api comment updating, and simplifying

--- app/models/stack.rb
+++ app/models/stack.rb
@@ -5,19 +5,17 @@ class Stack < ActiveRecord::Base
   validates :api_id, :name, :slug, :popularity, presence: true
   validates :api_id, uniqueness: true
 
-  # Updates the DB so that the layers are the same as the ones in the API.
+  # Syncs up stacks from the StackShare API
   def self.sync_from_stackshare_api(tag_id)
     stack_share_service = StackShareService.new
 
     # First, getting everything from the DB
     all_stacks_from_db = Stack.all
 
-    # Then, making as many lookup calls as there are layers, in order to accumulate all the tools with a layer
-    all_stacks_from_api = []
     # Calling the API for just this page
     res = stack_share_service.call_api '/stacks/lookup', tag_id: tag_id
-    raise "Error when calling StackShare's API to fetch layers: #{res}" unless res.is_a?(Net::HTTPSuccess)
-    all_stacks_from_api += JSON.parse(res.body)
+    raise "Error when calling StackShare's API to fetch stacks: #{res}" unless res.is_a?(Net::HTTPSuccess)
+    all_stacks_from_api = JSON.parse(res.body)
 
     # Then, we want to modify the Hash from the API a bit, so that fields match exactly (like the tools and tags fields)
     # TODO use the nice method
--- app/models/tool.rb
+++ app/models/tool.rb
@@ -5,19 +5,19 @@ class Tool < ActiveRecord::Base
   validates :api_id, :name, :slug, :popularity, :layer_id, presence: true
   validates :api_id, uniqueness: true
 
-  # Updates the DB so that the layers are the same as the ones in the API.
+  # Syncs up tools from the StackShare API
   def self.sync_from_stackshare_api
     stack_share_service = StackShareService.new
 
     # First, getting everything from the DB
     all_tools_from_db = Tool.all
 
-    # Then, making as many lookup calls as there are layers, in order to accumulate all the tools with a layer
+    # Then, making as many lookup calls as there are tools, in order to accumulate all the tools with a layer
     all_tools_from_api = []
     Layer.pluck(:api_id).each do |layer_id|
       # Calling the API for just this page
       res = stack_share_service.call_api '/tools/lookup', layer_id: layer_id
-      raise "Error when calling StackShare's API to fetch layers: #{res}" unless res.is_a?(Net::HTTPSuccess)
+      raise "Error when calling StackShare's API to fetch tools: #{res}" unless res.is_a?(Net::HTTPSuccess)
       all_tools_from_api += JSON.parse(res.body)
     end
 
