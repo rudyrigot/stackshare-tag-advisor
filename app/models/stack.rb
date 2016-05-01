a6f57a0484e368c6147068496ba5d1aa92848834
Rudy Rigot <rudy.rigot@gmail.com>
2016-05-01 12:08:48 -0700

    Stack::sync_from_stackshare_api

--- app/models/stack.rb
+++ app/models/stack.rb
@@ -1,4 +1,34 @@
 class Stack < ActiveRecord::Base
   validates :api_id, :name, :slug, :popularity, presence: true
   validates :api_id, uniqueness: true
+
+  # Updates the DB so that the layers are the same as the ones in the API.
+  def self.sync_from_stackshare_api(tag_id)
+    stack_share_service = StackShareService.new
+
+    # First, getting everything from the DB
+    all_stacks_from_db = Stack.all
+
+    # Then, making as many lookup calls as there are layers, in order to accumulate all the tools with a layer
+    all_stacks_from_api = []
+    # Calling the API for just this page
+    res = stack_share_service.call_api '/stacks/lookup', tag_id: tag_id
+    raise "Error when calling StackShare's API to fetch layers: #{res}" unless res.is_a?(Net::HTTPSuccess)
+    all_stacks_from_api += JSON.parse(res.body)
+
+    # Then, we want to modify the Hash from the API a bit, so that fields match exactly (like the tools and tags fields)
+    # TODO use the nice method
+    all_stacks_from_api.map! do |stack|
+      {
+        "id" => stack["id"],
+        "name" => stack["name"],
+        "slug" => stack["slug"],
+        "popularity" => stack["popularity"],
+        "full_object" => stack
+      }
+    end
+
+    # Finally, syncing it all
+    stack_share_service.sync_all(Stack, all_stacks_from_db, all_stacks_from_api, [:name, :slug, :popularity, :full_object])
+  end
 end
--- test/models/stack_test.rb
+++ test/models/stack_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class StackTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    # Stubbing the /stacks/lookup requests
+    stacks_json = %([{"id":2,"name":"Alphabet","slug":"alphabet","popularity":170},{"id":3,"name":"Holberton School","slug":"holberton-school","popularity":4}])
+    stub_request(:get, "https://api.stackshare.io/v1/stacks/lookup?access_token=&tag_id=1").to_return(body: stacks_json)
+  end
+
+  test "sync_from_stackshare_api" do
+    Stack.sync_from_stackshare_api(1)
+    expected = ["alphabet", "holberton-school"]
+    assert_equal expected, Stack.order(:api_id).pluck(:slug)
+  end
 end
