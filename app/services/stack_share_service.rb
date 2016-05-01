97a4a0dba0f1d1edf1a286939b88b9679b2e5680
Rudy Rigot <rudy.rigot@gmail.com>
2016-05-01 10:30:01 -0700

    Move some StackShareService methods as private + test of StackShareMethod#object_from_api_id

--- app/services/stack_share_service.rb
+++ app/services/stack_share_service.rb
@@ -13,55 +13,6 @@ class StackShareService
   end
 
 
-  ## USED FOR ALL SYNCS
-
-  # Given a model class and an API id, returns the matching object from DB. Returns nil if such object doesn't exist.
-  # We could have used model_class.find_by(api_id: api_id), but this operation is done countless times in a row during a sync,
-  # for each object to insert or update. Better to just create a Hash that will index things from one DB call.
-  #
-  # Sometimes, you already have the result of the DB query before calling this, if so, you can pass it as the collection
-  # parameter and not even need to call the DB; if you don't pass one, the DB will be queried.
-  #
-  # @param [Model] model_class the model class that we need to look in
-  # @param [FixNum] api_id the api_id to look up
-  # @param [Array<Model>] collection the collection of all objects of that type in DB, if you have it (otherwise DB will be queried)
-  def object_from_api_id(model_class, api_id, collection = nil)
-    # Fetching all objects of the model, if no collection parameter
-    collection ||= model_class.all
-    # Building the index, if didn't exist yet
-    unless @objects_by_api_id_by_model_class.has_key? model_class
-      @objects_by_api_id_by_model_class[model_class] = Hash[collection.map{|item| [item.api_id, item] }]
-    end
-    # Lookup
-    @objects_by_api_id_by_model_class[model_class][api_id]
-  end
-
-  # Syncs up all objects in a DB based on those fetched from the API.
-  # Updates are incremental, so that users' experiences are not disrupted during the update.
-  #
-  # @param [class] model_class the model class that should be used to perform ActiveRecord operations
-  # @param [Array<Model>] all_from_db all of those objects as they currently are in the DB
-  # @param [Array<Hash>] all_from_api all of those objects as fetched from the API
-  # @param [Array<Symbol>] fields the fields that must be synced up, besides api_id
-  def sync_all!(model_class, all_from_db, all_from_api, fields = [:name])
-    # Destroying all records that shouldn't be there
-    api_ids_to_delete = all_from_db.map(&:api_id) - all_from_api.map{|tag| tag["id"]}
-    model_class.where(api_id: api_ids_to_delete).destroy_all
-
-    # And for all records that should be there, either inserting or updating
-    all_from_api.each do |record|
-      object_in_db = object_from_api_id(model_class, record["id"], all_from_db)
-      if object_in_db.present?  # Already exists, just needs to be potentially updated
-        object_in_db.update!(Hash[fields.map{|f|[f, record[f.to_s]]}])
-      else  # Doesn't exist, needs to be created
-        object_hash = {api_id: record["id"]}
-        fields.each{|f| object_hash[f] = record[f.to_s] }
-        model_class.create!(object_hash)
-      end
-    end
-  end
-
-
   ## TAGS
 
   # Updates the DB so that the tags are the same as the ones in the API.
@@ -135,6 +86,58 @@ class StackShareService
 
     # Finally, syncing it all
     sync_all!(Tool, all_tools_from_db, all_tools_from_api, [:name, :slug, :popularity, :layer_id])
+  end
+
+
+  private
+
+
+  ## USED FOR ALL SYNCS
+
+  # Given a model class and an API id, returns the matching object from DB. Returns nil if such object doesn't exist.
+  # We could have used model_class.find_by(api_id: api_id), but this operation is done countless times in a row during a sync,
+  # for each object to insert or update. Better to just create a Hash that will index things from one DB call.
+  #
+  # Sometimes, you already have the result of the DB query before calling this, if so, you can pass it as the collection
+  # parameter and not even need to call the DB; if you don't pass one, the DB will be queried.
+  #
+  # @param [Model] model_class the model class that we need to look in
+  # @param [FixNum] api_id the api_id to look up
+  # @param [Array<Model>] collection the collection of all objects of that type in DB, if you have it (otherwise DB will be queried)
+  def object_from_api_id(model_class, api_id, collection = nil)
+    # Fetching all objects of the model, if no collection parameter
+    collection ||= model_class.all
+    # Building the index, if didn't exist yet
+    unless @objects_by_api_id_by_model_class.has_key? model_class
+      @objects_by_api_id_by_model_class[model_class] = Hash[collection.map{|item| [item.api_id, item] }]
+    end
+    # Lookup
+    @objects_by_api_id_by_model_class[model_class][api_id]
+  end
+
+  # Syncs up all objects in a DB based on those fetched from the API.
+  # Updates are incremental, so that users' experiences are not disrupted during the update.
+  #
+  # @param [class] model_class the model class that should be used to perform ActiveRecord operations
+  # @param [Array<Model>] all_from_db all of those objects as they currently are in the DB
+  # @param [Array<Hash>] all_from_api all of those objects as fetched from the API
+  # @param [Array<Symbol>] fields the fields that must be synced up, besides api_id
+  def sync_all!(model_class, all_from_db, all_from_api, fields = [:name])
+    # Destroying all records that shouldn't be there
+    api_ids_to_delete = all_from_db.map(&:api_id) - all_from_api.map{|tag| tag["id"]}
+    model_class.where(api_id: api_ids_to_delete).destroy_all
+
+    # And for all records that should be there, either inserting or updating
+    all_from_api.each do |record|
+      object_in_db = object_from_api_id(model_class, record["id"], all_from_db)
+      if object_in_db.present?  # Already exists, just needs to be potentially updated
+        object_in_db.update!(Hash[fields.map{|f|[f, record[f.to_s]]}])
+      else  # Doesn't exist, needs to be created
+        object_hash = {api_id: record["id"]}
+        fields.each{|f| object_hash[f] = record[f.to_s] }
+        model_class.create!(object_hash)
+      end
+    end
   end
 
 end
--- test/models/services/stackshare_service_test.rb
+++ test/models/services/stackshare_service_test.rb
@@ -47,4 +47,12 @@ class StackShareServiceTest < ActiveSupport::TestCase
     assert_equal expected, Tool.order(:api_id).map{|t|t.layer.slug}
   end
 
+  test "object_from_api_id" do
+    sss = StackShareService.new
+    assert_equal 'cloud', sss.send(:object_from_api_id, Tag, 2).name
+    assert_equal 'new_thing', sss.send(:object_from_api_id, Tag, 9).name
+
+    assert_equal 'cloud', StackShareService.new.send(:object_from_api_id, Tag, 2, Tag.all).name
+  end
+
 end
