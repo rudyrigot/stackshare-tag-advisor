cab7563120c7ad383809606b65d72e6fc27fad89
Rudy Rigot <rudy.rigot@gmail.com>
2016-05-01 12:42:15 -0700

    StackShareService#object_from_api_id test if nil

--- test/models/services/stackshare_service_test.rb
+++ test/models/services/stackshare_service_test.rb
@@ -7,10 +7,11 @@ class StackShareServiceTest < ActiveSupport::TestCase
 
   test "object_from_api_id" do
     sss = StackShareService.new
-    assert_equal 'cloud', sss.send(:object_from_api_id, Tag, 2).name
-    assert_equal 'new_thing', sss.send(:object_from_api_id, Tag, 9).name
+    assert_equal 'cloud', sss.object_from_api_id(Tag, 2).name
+    assert_equal 'new_thing', sss.object_from_api_id(Tag, 9).name
+    assert_nil sss.object_from_api_id(Tag, 999)
 
-    assert_equal 'cloud', StackShareService.new.send(:object_from_api_id, Tag, 2, Tag.all).name
+    assert_equal 'cloud', StackShareService.new.object_from_api_id(Tag, 2, Tag.all).name
   end
 
   test "sync_all" do
